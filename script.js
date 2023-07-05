08df486609496cbff4a76d63ec5e1b98d8b59852
Simran Johal <134616481+simran-johal@users.noreply.github.com>
2023-07-05 14:24:32 +0100

    Make the basic game functionality complete
    - Play 5 rounds each round presents a winner
    - Present a final winner with the score

--- script.js
+++ script.js
@@ -1,63 +1,94 @@
 
 
 
-
-function getComputerChoice() {
+    // these are the functional blocks to get the game to do something based on the interactive mechanism
+    function getComputerChoice() {
     let stringArray = ["rock", "paper", "scissors"]
     let randomString = Math.floor(Math.random() * stringArray.length) 
     return stringArray[randomString]
     
 }
-getComputerChoice()
-
-
-
-
-for (let i = 0; i <= 5; i++) {
+    getComputerChoice()
     
     function playRound (playerSelection, computerSelection){
-    
+
     if (playerSelection == "rock" && computerSelection == "paper") {
         return "You Lose!, Paper beats Rock"
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
         return "You Win!, Rock beats Scissors"
     } else if (playerSelection == "rock" && computerSelection == "rock") {
         return "A draw!, Try Again"
+
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         return "You Win!, Paper beats Rock"
     } else if (playerSelection == "paper" && computerSelection == "scissors"){
         return "You Lose!, Scissors beats Paper"
     } else if (playerSelection == "paper" && computerSelection == "paper") {
         return "A draw!, Try Again"
+
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         return "You Win!, Scissors beats Paper"
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
         return "You Lose!, Rock beats Scissors"
     } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+
         return "A draw!, Try Again"
-    } else return "Whoopsy, something went wrong, try again!"
-    }
-        if (playRound("You Win!, Rock beats Scissors" || "You Win!, Paper beats Rock" || "You Win!, Scissors beats Paper")) {
-            return "Player Wins!"
-            let playerScore = ++
+    } else return "Whoopsy, something went wrong, try again!" }
+     
 
-        } else if (playRound("You Lose!, Paper beats Rock" || "You Lose!, Scissors beats Paper" || "You Lose!, Rock beats Scissors"))
-            return "Computer Wins!"
-            let computerScore = ++
 
-        } else return "Must be a draw!, try again"   
 
 let playerScore = 0
 let computerScore = 0
+let winnerOfRound = ""
+let finalWinner = "";
+
+async function game() {
+// this is the interactive mechanism 
+    
+for (let i = 0; i < 5; i++) {
+    
+    const playerInput = await window.prompt("Choose Rock, Paper or Scissors")
+    const playerChoice = playerInput.toLowerCase()
+    const computerChoice = getComputerChoice()
+    const roundResult = playRound(playerChoice,computerChoice)
+
+        if (roundResult == "You Win!, Rock beats Scissors" || 
+            roundResult == "You Win!, Paper beats Rock" || 
+            roundResult == "You Win!, Scissors beats Paper") {
+            winnerOfRound = "Player Wins!"
+            playerScore += 1;
+
+        } else if (roundResult == "You Lose!, Paper beats Rock" || 
+               roundResult == "You Lose!, Scissors beats Paper" || 
+               roundResult == "You Lose!, Rock beats Scissors") {
+                winnerOfRound = "Computer Wins!"
+                computerScore += 1;
+
+        } else winnerOfRound = "Must be a draw!, try again"
+    
+        console.log(winnerOfRound)
+        console.log(playerScore)
+        console.log(computerScore)
 
     }
 
 
-const playerInput = "rock"
-const playerChoice = playerInput.toLowerCase()
-const computerChoice = getComputerChoice()
+    if (playerScore > computerScore) {
+        finalWinner = "You Win! " + playerScore + computerScore;
+    } else if (playerScore < computerScore) {
+        finalWinner = "You Lose! " + playerScore + computerScore
+    } else finalWinner = "It's draw! try again!"
 
-console.log(playRound(playerChoice, computerChoice))
+    console.log(finalWinner)
+
+
+}
+
+
+game()
+
+
 
 
 
